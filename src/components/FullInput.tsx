adf07ea508cca0f32ba83cd903ff22983fbfecae
Tatiana Kedrova <Baletrot@gmail.com>
2026-05-31 15:46:58 +0300

    new error useState for empty input field

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,13 +1,28 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import TaskItem from "./TaskItem";
 import { useTodolistStore } from "../store.zustand/useTodolistStore";
 
 export const FullInput = () => {
   let [title, setTitle] = useState<string>("");
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  let [hasError, setHasError] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const tasks = useTodolistStore((state) => state.tasks);
   const filter = useTodolistStore((state) => state.filter);
   const addNewTask = useTodolistStore((state) => state.addNewTask);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, []);
+  timeoutRef.current = window.setTimeout(() => {
+    setHasError(false);
+  }, 5000);
 
   const filteredTasks = useMemo(() => {
     switch (filter) {
@@ -19,20 +34,39 @@ export const FullInput = () => {
         return tasks;
     }
   }, [tasks, filter]);
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.currentTarget.value);
   };
+  const isInputEmpty = () => {
+    const isEmpty = title.trim() === "";
+    setHasError(isEmpty);
+    return isEmpty;
+  };
   const handleAddTask = () => {
+    if (title.trim() === "") {
+      setHasError(true);
+
+      // Clear any older running timer so they don't fight each other
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      timeoutRef.current = setTimeout(() => {
+        setHasError(false); // Turn off the error state after 5000ms
+      }, 5000);
+
+      return;
+    }
     addNewTask(title);
     setTitle("");
+    setHasError(false);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
   };
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isInputEmpty()) return;
     if (event.key === "Enter") {
       addNewTask(title);
       inputRef.current?.blur();
@@ -42,7 +76,9 @@ export const FullInput = () => {
 
   return (
     <div>
+      {hasError && <p id="error-message">Title is required</p>}
       <input
+        className={hasError ? "error" : ""}
         ref={inputRef}
         type="text"
         placeholder="Type a new message"
